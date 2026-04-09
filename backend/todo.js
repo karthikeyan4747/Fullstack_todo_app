8ea370967ada7715c8f60c0a44ab76c0fd8299f5
karthikeyan4747 <karthikeyan53932@gmail.com>
2026-04-09 22:39:40 +0530

    Addedn  extra features

--- backend/todo.js
+++ backend/todo.js
@@ -19,17 +19,42 @@ db.connect((err) => {
 })
 app.get('/todos', (req, res) => {
     db.query('select * from todos', (err, result) => {
-        if (err) return res.status(500).json({'Message':'Error lil bro'})
+        if (err) return res.status(500).json({ 'Message': 'Error lil bro' })
         res.json(result);
     })
 })
 
-app.post('/todos',(req,res)=>{
+app.post('/todos', (req, res) => {
     const todo = req.body.task;
     const status = req.body.status;
-    db.query('insert into todos(task,status) values(?,?)',[todo,status],(err,result)=>{
-        if (err) return res.status(500).json({'Message':"Error lil bro"})
-        res.json({'Task':todo,'Status':status})
+    db.query('insert into todos(task,status) values(?,?)', [todo, status], (err, result) => {
+        if (err) return res.status(500).json({ 'Message': "Error lil bro" })
+        res.json({ 'Task': todo, 'Status': status })
+    })
+})
+
+app.get('/todos/:id', (req, res) => {
+    const id = Number(req.params.id);
+    db.query('select * from todos where id = (?)', [id], (err, result) => {
+        if (err) return res.status(500).json({ 'Message': "Error lil bro" });
+        res.json(result);
+    })
+})
+
+app.put('/todos/:id', (req, res) => {
+    const status = req.body.status;
+    const id = Number(req.params.id);
+
+    db.query('update todos set status = ? where id = ?', [status, id], (err, result) => {
+        if (err) return res.status(500).json({ 'Message': "Error lil bro" });
+        res.json({ Message: "Alu" });
+    })
+})
+
+app.delete('/todos/:id', (req, res) => {
+    db.query('delete from todos where id = ?', [Number(req.params.id)], (err, result) => {
+        if (err) return res.status(500).json({ 'Message': "Error lil bro" });
+        res.json({ Message: "Alu" });
     })
 })
 
--- frontend/todo/src/App.jsx
+++ frontend/todo/src/App.jsx
@@ -8,7 +8,11 @@ import axios from 'axios'
 function App() {
   const [todos,setTodo] = useState([]);
   const [task,setTask] = useState('');
-
+  const [id,setId] = useState('')
+  const [specific,setSpecific] = useState([])
+  const [update,setUpdate] = useState([])
+  const [status,setStatus] = useState('')
+  const [deletee,setDelete] = useState('')
   const fetchTodo = () =>{
     axios.get('http://localhost:3000/todos').then(res => setTodo(res.data))
   };
@@ -24,21 +28,56 @@ function App() {
     });
   };
 
+  const getSpecific = () =>{
+    axios.get(`http://localhost:3000/todos/${id}`).then(res=>setSpecific(res.data))
+  };
+
+  const updateStatus = () => {
+    axios.put(`http://localhost:3000/todos/${update}`,{status}).then(()=>{
+      setUpdate('');
+      setStatus('')
+      fetchTodo();
+    })
+  }
+
+  const deletetodo = () => {
+    axios.delete(`http://localhost:3000/todos/${deletee}`).then(()=>{
+      setDelete('');
+      fetchTodo();
+    })
+  }
+
   return(
   <div>
     <h1>TO-DO LIST</h1>
     <label htmlFor='task' >Task : </label>
     <input id='task' value={task} onChange={(e)=> setTask(e.target.value)} placeholder='Enter a Task' /><br /> <br />
     <button onClick={addTodo}>Add task</button>
+    <h2>Get specific To-Do using ID</h2>
+
+    <input value={id} onChange={(e)=>setId(e.target.value)} />
+    <button onClick={getSpecific}>Get todo</button>
+    <h3>{specific.map(specifics=>(
+      <li key={specifics.id}>id:{specifics.id}   Task: {specifics.task}    Status: {specifics.status}</li>
+    ))}</h3>
+    <h2>Update status using id</h2>
+    <label htmlFor='val' >ID : </label>
+    <input value={update} id='val' onChange={(e)=>setUpdate(e.target.value)} /><br /> <br />
+     <label htmlFor='sta' >Status : </label>
+    <input value={status} id='sta' onChange={(e)=>setStatus(e.target.value)} /><br /> <br />
+    <button onClick={updateStatus}>Update</button>
+    <h2>Delete todo using id</h2>
+    <label htmlFor='del-id' >ID : </label>
+    <input value={deletee} id='del-id' onChange={(e)=>setDelete(e.target.value)} /><br /> <br />
+    <button onClick={deletetodo}>Delete</button>
+    <h1>TO-DO list</h1>
     <ul>
       {todos.map(todo =>(
-        <li key={todo.id}>Task: {todo.task}    Status: {todo.status}</li>
+        <li key={todo.id}>id:{todo.id}   Task: {todo.task}    Status: {todo.status}</li>
       ))}
     </ul>
   </div>
   )
-
-  
   
 }
 
